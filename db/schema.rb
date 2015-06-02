23f1cda8d10f177abe85031bf27e93e96c591534
dathi <dathi@tma.com.vn>
2015-06-02 22:48:00 +0700

    add 'done' column to Item

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602102910) do
+ActiveRecord::Schema.define(version: 20150602154432) do
 
   create_table "items", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "done"
   end
 
 end
