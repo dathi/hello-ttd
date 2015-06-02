ca6664a842892d472d11c70e9610e8f4451512c0
dathi <dathi@tma.com.vn>
2015-06-02 17:44:38 +0700

    create feature file and declare scenario, create step defs file, add steps graduately, run 'rake', add model, add root path to route, add controller, add action and view

---
+++ features/step_definitions/todo_list_steps.rb
@@ -0,0 +1,8 @@
+Given "I have some items in my to-do list" do
+  @item_1 = Item.create name: "Make a Rails app"
+  @item_2 = Item.create name: "Demonstrate test-driven development"
+end
+
+When "I go to the home page" do
+	visit root_path
+end
