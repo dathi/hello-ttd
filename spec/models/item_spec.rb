a6242736cd83cb24c57d889a9ef241da460f5571
dathi <dathi@tma.com.vn>
2015-06-02 22:43:46 +0700

    add a spec for mark_done

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".mark_done" do
+  	it "marks the specified item as done" do
+  		item = Item.create name: "item A"
+  		Item.mark_done item.id
+  		expect(item.reload).to be_done
+  	end
+  end
 end
