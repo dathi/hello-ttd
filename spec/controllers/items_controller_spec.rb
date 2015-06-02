62b6fc67fa6b7129aed065a59e41c1714e2ac3a6
dathi <dathi@tma.com.vn>
2015-06-02 17:58:51 +0700

    add testcase for get index, this will be failed

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
-
+	let(:items) { [double(:items)] }
+	
+	describe "GET index" do
+		it "assign all the items for the view" do
+			get :index
+			expect(assigns[:items]).to eq items
+		end
+	end
 end
