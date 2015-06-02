c1d36a1e74dd3f1b942d727e0b38f6b24b1607c5
dathi <dathi@tma.com.vn>
2015-06-02 22:30:40 +0700

    failling spec for mark_done marking an item as done

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -35,8 +35,17 @@ RSpec.describe ItemsController, type: :controller do
 	end
 
 	describe "POST mark_done" do
+		before do
+			allow(Item).to receive :mark_done
+		end
+
 		def do_mark_done
 			post :mark_done, id: "1"
+		end
+
+		it "marks item as done" do
+			do_mark_done
+			expect(Item).to have_received(:mark_done).with "1"
 		end
 
 		it "redirect to the index" do
